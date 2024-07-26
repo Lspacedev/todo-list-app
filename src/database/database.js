80f96c468b93c2a4fd63d8f7bd9ae72b37043f25
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-07-26 15:51:38 +0200

    updated sql database, initDb

--- src/database/database.js
+++ src/database/database.js
@@ -2,30 +2,48 @@ import { useState, useEffect } from "react";
 import initSqlJs from "sql.js";
 
 function Database() {
+  const [users, setUsers] = useState(["a", "b", "c", "d", "e"]);
   const [db, setDb] = useState(null);
 
   const [error, setError] = useState(null);
   const initDb = async () => {
-    let datab;
-    const SQL = await initSqlJs({
-      // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
-      // You can omit locateFile completely when running in node
-      locateFile: (file) => `https://sql.js.org/dist/${file}`,
-    });
-    datab = new SQL.Database();
-    //setDb(new SQL.Database());
-    let sqlstr =
-      "CREATE TABLE user (a int, b char); \
-INSERT INTO user VALUES (0, 'hello'); \
-INSERT INTO user VALUES (1, 'world');";
-    datab.run(sqlstr);
-    let arr = datab.exec("select * from user");
-    console.log(arr);
-    setDb(arr);
+    //let datab;
+    try {
+      const SQL = await initSqlJs({
+        // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
+        // You can omit locateFile completely when running in node
+        locateFile: (file) => `https://sql.js.org/dist/${file}`,
+      });
+      let datab = new SQL.Database();
+      //setDb(new SQL.Database());
+      let user1 = users[2];
+      let user2 = users[3];
+      /*let sqlstr = `CREATE TABLE user (id int, name char); \"`;
+
+      console.log(sqlstr);
+      users.forEach((user, i) => {
+        console.log(user);
+        sqlstr += ` INSERT INTO user VALUES (${i}, ${user}); \"`;
+      });*/
+
+      let sqlstr = `CREATE TABLE user (id int, name char); \
+        INSERT INTO user VALUES (0, 'hello'); \
+        INSERT INTO user VALUES (1, 'world'); \
+        
+        `;
+      console.log(sqlstr);
+      datab.run(sqlstr);
+      let arr = datab.exec("select * from user");
+      console.log(datab);
+      console.log(arr);
+      setDb(arr);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => initDb, []);
-  console.dir(db);
+  //console.dir(db);
   /*let sqlstr =
     "CREATE TABLE hello (a int, b char); \
 INSERT INTO hello VALUES (0, 'hello'); \
